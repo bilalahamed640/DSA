e3bba6eeea4ded3150172face3ec05a2f17a4e95
bilal <bilalahamed1998@gmail.com>
2024-12-11 12:40:02 +0530

    Bubble sort update

--- Sorting algoritms/Bubble.py
+++ Sorting algoritms/Bubble.py
@@ -4,7 +4,7 @@ def bubble_sort():
     for i in range(n-1):
         swapped=False
         for j in range(n-i-1):
-           if arr[j+1]<rr[j]:
+           if arr[j+1]<arr[j]:
                arr[j],arr[j+1]=arr[j+1],arr[j]
                swapped=True
         if not swapped:
